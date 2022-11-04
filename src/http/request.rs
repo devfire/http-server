4c37084692f56a7a16fc7f6eb60c2655eaa3994e
Igor <igor@devfire.io>
2022-11-04 16:25:05 -0400

    added traits and custom error type enum

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,30 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+// NOTE: Result<> expects to return itself and an error message
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        unimplemented!();
+    }
+}
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    // sample request:
+    // GET /search HTTP/1.1
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!();
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
+use crate::http::Request;
 use std::io::Read;
+use std::convert::TryFrom;
 
 pub struct Server {
     addr: String,
@@ -26,6 +28,13 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received: {}", String::from_utf8_lossy(&buffer));
+                            // two .. mean "entire array"
+                            // compiler cannot do auto-conversion, we must explicitly convert
+                            // [..] -- byte slice, entire array
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => {println!("ERROR: failed to parse the request, {}", e)}
+                            }
                         }
                         Err(e) => println!("ERROR: failed to read from conn {}", e),
                     }
