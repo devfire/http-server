593f0e2a746b8a6e6677d27c2e8ac011dbb21da0
Igor <igor@devfire.io>
2022-11-04 12:01:04 -0400

    working stubs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let server = Server::new("0.0.0.0:8080".to_string());
+    // server.run();
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server{
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
 }
