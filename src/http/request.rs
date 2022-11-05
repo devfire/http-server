b73e3ce4a7e6cdbb3a610bc848505cee28b2b0ae
Igor <igor@devfire.io>
2022-11-05 15:32:12 -0400

    wrapped up module on parsing

--- src/http/request.rs
+++ src/http/request.rs
@@ -32,6 +32,12 @@ impl TryFrom<&[u8]> for Request {
         // Shadowing request - request
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+        
         unimplemented!()
     }
 }
